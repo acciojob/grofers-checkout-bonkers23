5661c1616ee4bc2b4699cd902d066a63c5064041
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-05-05 22:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const getSumBtn = document.createElement("button");
+let prices = document.getElementsByClassName("price");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-  const priceCells = document.querySelectorAll(".price");
-  let total = 0;
+	let totalPrice=0;
+	for(let i = 0 ; i<prices.length ; i++){
+		totalPrice+=Number(prices[i].innerText);
+	}
 
-  priceCells.forEach(cell => {
-    total += Number(cell.textContent);
-  });
-
-  const totalRow = document.createElement("tr");
-  const totalCell = document.createElement("td");
-  totalCell.setAttribute("colspan", 2); // spans both columns
-  totalCell.textContent = `Total Price: Rs ${total}`;
-  totalRow.appendChild(totalCell);
-
-  const table = document.querySelector("table");
-  table.appendChild(totalRow);
+	let newRow = document.createElement("tr");
+	newRow.id = 'ans';
+	newRow.innerText = totalPrice;
+	document.body.appendChild(newRow);
+  
 };
 
 getSumBtn.addEventListener("click", getSum);
